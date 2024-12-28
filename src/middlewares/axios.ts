f992f56d9c13a950e7c4dd2ca06d7c61529c37d9
BlurSniper <artemmarharyt@gmail.com>
2024-12-28 09:38:21 -0800

    first

---
+++ src/middlewares/axios.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const baseURL = "http://localhost:8000/api/v1";
+
+const instance = axios.create({
+  baseURL: baseURL,
+  withCredentials: true,
+});
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 403) {
+      if (!originalRequest._retry) {
+        originalRequest._retry = true;
+        return Promise.reject(error);
+      } else {
+        window.location.href = "/login";
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
